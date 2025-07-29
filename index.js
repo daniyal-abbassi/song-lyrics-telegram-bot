db7a6a8805e3d9b9b686265d8a9173d28367a49c
daniyal-abbassi <slimshady3753@protonmail.com>
2025-07-30 02:16:12 +0330

    chore: Setup basic bot skeleton

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 // load the .env file
 require('dotenv').config();
 // import telegraf
-
+const {Telegraf} = require('telegraf');
 // create bot
-
+const bot = new Telegraf(process.env.BOT_TOKEN);
 // a handler for /start command
-
+bot.start(async(ctx)=>{
+    await ctx.reply('Send me a message')
+})
 // handler for text message
-
+bot.on('text',async(ctx) => {
+    const userInput = ctx.message.text;
+    await ctx.reply(`You Just Saied: ${userInput}`)
+})
 // launch the bot
-
+bot.launch();
+console.log('The Bot is Running...')
 // stop handling
+process.once('SIGINT',() => bot.stop('SIGING'));
+process.once('SIGTERM',() => bot.stop('SIGTERM'));

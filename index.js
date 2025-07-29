7b6985514c0ffd5fa54e8f0b233977695427245d
daniyal-abbassi <slimshady3753@protonmail.com>
2025-07-30 02:11:00 +0330

    chore: install telegraf, setup skeleton

---
+++ index.js
@@ -0,0 +1,13 @@
+// load the .env file
+require('dotenv').config();
+// import telegraf
+
+// create bot
+
+// a handler for /start command
+
+// handler for text message
+
+// launch the bot
+
+// stop handling

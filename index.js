de46b437acd0ad207602364d2dc924a691deab23
daniyal-abbassi <slimshady3753@protonmail.com>
2025-07-30 05:54:19 +0330

    fix: use gemini instead of deepseek

--- index.js
+++ index.js
@@ -2,13 +2,10 @@
 require("dotenv").config();
 // import telegraf
 const { Telegraf } = require("telegraf");
-// import openai
-const { OpenAI } = require("openai");
-// --- CONFIGURE DEEPSEEK ---
-const openai = new OpenAI({
-  baseURL: "https://api.deepseek.com/v1",
-  apiKey: process.env.DEEPSEEK_TOKEN,
-});
+// import gemini
+const { GoogleGenAI } = require("@google/genai");
+// --- CONFIGURE GEMINI ---
+const ai = new GoogleGenAI(process.env.GEMINI_API_KEY);
 
 // create bot
 const bot = new Telegraf(process.env.BOT_TOKEN);
@@ -23,14 +20,16 @@ bot.on("text", async (ctx) => {
     // show typing indicator to the user
     await ctx.telegram.sendChatAction(ctx.chat.id, "typing");
     // USE AI
-    const completion = await openai.chat.completions.create({
-      messages: [
-        { role: "system", content: "You are a happy friend who gives short answares."},
-        { role: "user", content: userInput }
-      ],
-      model: "deepseek-chat",
-    });
-    ctx.reply(completion.choices[0].message.content);
+   const geminiResponse = await ai.models.generateContent({
+    model: "gemini-2.5-flash",
+    contents: userInput,
+    config: {
+      thinkingConfig: {
+        thinkingBudget: 0, // disable thinking ability - more spped and less token usage
+      }
+    }
+   })
+    ctx.reply(geminiResponse.text);
   } catch (error) {
     console.error("AI error: ", error);
     ctx.reply("Sorry, ai is fucked at the moment, do the thinking yourself...");

d43af6e26dc512c20b11a730651c2bd4f20eb665
daniyal-abbassi <slimshady3753@protonmail.com>
2025-07-30 19:29:39 +0330

    fix: Use googleSearch with Gemini

--- index.js
+++ index.js
@@ -46,10 +46,10 @@ bot.command("lyrics", async (ctx) => {
     const URL_a='https://www.azlyrics.com/lyrics/melaniemartinez/void.html';
     const URL_b='https://www.azlyrics.com/lyrics/jcole/portantonio.html';
     const URL_c='https://www.azlyrics.com/lyrics/marilynmanson/killingstrangers.html'
-    let URL=`https://www.azlyrics.com/lyrics/${artistName.toLocaleLowerCase.trim()}/${songName}.html`;
+    let URL=`https://www.azlyrics.com/lyrics/${artistName.toLocaleLowerCase().replace(/\s+/g,'')}/${songName.toLocaleLowerCase().replace(/\s+/g,"")}.html`;
     console.log('this is url: ',URL)
     const getLyricsPrompt = `
-      you are a find and retrieve bot, you search sites like azlyrics: https://www.azlyrics.com/lyrics/${artistName.toLocaleLowerCase.trim()}/${songName}.html to get lyrics of song [${songName}] by [${artistName}], do not generete yourself 
+      you are a find and retrieve bot, you search sites like azlyrics: ${URL} to get lyrics of song [${songName}] by [${artistName}], do not generete yourself 
       
       **Output Format:**
       - If you find the verified lyrics, provide ONLY the lyrics and nothing else.
@@ -60,12 +60,12 @@ bot.command("lyrics", async (ctx) => {
       model: "gemini-2.5-flash",
       contents: getLyricsPrompt,
       config: {
-        tools: [{urlContext: {}}],
+        tools: [{urlContext: {},googleSearch: {}}],
         temperature: 0.1,
       },
     });
     // test
-    console.log('this should be metadata: ',lyrics.candidates)
+    console.log('this should be metadata: ',lyrics.candidates[0].urlContextMetadata)
     await ctx.reply(lyrics.text);
   } catch (error) {
     console.error("AI error: ", error);

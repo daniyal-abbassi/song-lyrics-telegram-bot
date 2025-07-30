a381f8ab221768205c778b4ff084e9c72ff35a6b
daniyal-abbassi <slimshady3753@protonmail.com>
2025-07-30 05:21:01 +0330

    fix: Use deepseek api instead of huggingface

--- index.js
+++ index.js
@@ -1,49 +1,44 @@
 // load the .env file
-require('dotenv').config();
+require("dotenv").config();
 // import telegraf
-const {Telegraf} = require('telegraf');
-
-// INITIALIZE HUGGING_FACE CLIENT
-
-let hf;
-
-async function initializeHf() {
-    const {InferenceClient} = await import('@huggingface/inference');
-    hf = new InferenceClient(process.env.Hugging_face_Token);
-    console.log('Hugging Face Client Initialized...')
-}
-
-initializeHf();
+const { Telegraf } = require("telegraf");
+// import openai
+const { OpenAI } = require("openai");
+// --- CONFIGURE DEEPSEEK ---
+const openai = new OpenAI({
+  baseURL: "https://api.deepseek.com/v1",
+  apiKey: process.env.DEEPSEEK_TOKEN,
+});
 
 // create bot
 const bot = new Telegraf(process.env.BOT_TOKEN);
 // a handler for /start command
-bot.start(async(ctx)=>{
-    await ctx.reply('Send me a message')
-})
+bot.start(async (ctx) => {
+  await ctx.reply("Send me a message");
+});
 // handler for text message
-bot.on('text',async(ctx) => {
-    const userInput = ctx.message.text;
-    try {
-        // show typing indicator to the user
-        await ctx.telegram.sendChatAction(ctx.chat.id,'typing');
-        // call Hugging Face conversational API
-        const response = await hf.conversational({
-            model: 'microsoft/DialoGPT-small',
-            inputs: {
-                text: userInput,
-            },
-        });
-        // send the AI's generated text 
-        ctx.reply(response.generated_text);
-    } catch (error) {
-        console.error('AI error: ',error);
-        ctx.reply('Sorry, ai is fucked at the moment, do the thinking yourself...')
-    }
-})
+bot.on("text", async (ctx) => {
+  const userInput = ctx.message.text;
+  try {
+    // show typing indicator to the user
+    await ctx.telegram.sendChatAction(ctx.chat.id, "typing");
+    // USE AI
+    const completion = await openai.chat.completions.create({
+      messages: [
+        { role: "system", content: "You are a happy friend who gives short answares."},
+        { role: "user", content: userInput }
+      ],
+      model: "deepseek-chat",
+    });
+    ctx.reply(completion.choices[0].message.content);
+  } catch (error) {
+    console.error("AI error: ", error);
+    ctx.reply("Sorry, ai is fucked at the moment, do the thinking yourself...");
+  }
+});
 // launch the bot
 bot.launch();
-console.log('The Bot is Running...')
+console.log("The Bot is Running...");
 // stop handling
-process.once('SIGINT',() => bot.stop('SIGING'));
-process.once('SIGTERM',() => bot.stop('SIGTERM'));
+process.once("SIGINT", () => bot.stop("SIGING"));
+process.once("SIGTERM", () => bot.stop("SIGTERM"));

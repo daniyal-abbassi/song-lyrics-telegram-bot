53fd43dfa2590ed80a998305921161c3be6cc0b7
daniyal-abbassi <slimshady3753@protonmail.com>
2025-08-01 04:55:05 +0330

    chore: Add Error classes comment

--- src/utils/customErrors.js
+++ src/utils/customErrors.js
@@ -1,4 +1,7 @@
 // ===== UTILITY - CUSTOM ERRORS =====
+/*
+* With this classes, you can catch and handle specific error types differently;
+**/
 class LyricsNotFoundError extends Error {
     constructor(message) {
         super(message);
